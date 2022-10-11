bbd45baeb50c660a5953373bcea06a0d528249ca
Guime81 <guillaume.devup@gmail.com>
2022-10-11 22:57:33 +0200

    redaction equipment.html + css + js

---
+++ script-equipment.js
@@ -0,0 +1,49 @@
+// Burger-Menu
+
+const bar1 = document.getElementById("bar1");
+const bar2 = document.getElementById("bar2");
+const bar3 = document.getElementById("bar3");
+const burgerMenu = document.getElementById("burger-menu");
+const menu = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", () =>{
+    bar1.classList.toggle("rotation");
+    bar2.classList.toggle("disparition");
+    bar3.classList.toggle("rotation2");
+    menu.classList.toggle("menu-list-active");
+});
+
+
+
+// Scroll To Top
+
+const scrollToTop = document.getElementById("scroll");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 110){
+        scrollToTop.style.display="block";
+    } else {
+        scrollToTop.style.display="none";
+    }
+});
+
+scrollToTop.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+});
+
+
+// Drop-down Article
+
+let arrows = document.querySelectorAll(".visible-content img");
+let displayContent = document.querySelectorAll(".invisible-content");
+
+arrows.forEach((arrow, index) => {
+    arrow.addEventListener("click", () => {
+        if (arrow.src.includes("down")) {
+            arrow.src = "medias/arrow-up.svg";
+        } else if (arrow.src.includes("up")) {
+            arrow.src = "medias/arrow-down.svg";
+        }
+        displayContent[index].classList.toggle("visible");
+    })
+});
