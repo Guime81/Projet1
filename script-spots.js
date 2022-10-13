0e17e4de87e4ebe5839d30a73fe1e467388e9c7b
Guime81 <guillaume.devup@gmail.com>
2022-10-13 13:45:09 +0200

    Clean code + create latest page and begin struture html and css mobile first

---
+++ script-spots.js
@@ -0,0 +1,32 @@
+// Burger-Menu
+
+const bar1 = document.getElementById("bar1");
+const bar2 = document.getElementById("bar2");
+const bar3 = document.getElementById("bar3");
+const burgerMenu = document.getElementById("burger-menu");
+const menu = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", () =>{
+    bar1.classList.toggle("rotation");
+    bar2.classList.toggle("disparition");
+    bar3.classList.toggle("rotation2");
+    menu.classList.toggle("menu-list-active");
+});
+
+
+// Scroll To Top
+
+const scrollToTop = document.getElementById("scroll");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 110){
+        scrollToTop.style.display="block";
+    } else {
+        scrollToTop.style.display="none";
+    }
+});
+
+scrollToTop.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+});
+
